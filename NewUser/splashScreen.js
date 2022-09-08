a47981805a3bf84c483dac166e960eaa3840de4f
Aditya Ibnu Arif <111210897+SiapaNamam@users.noreply.github.com>
2022-09-08 17:47:32 +0700

    Delete NewUser directory

--- NewUser/splashScreen.js
+++
@@ -1,9 +0,0 @@
-if(localStorage.getItem('localUsername')==null)
-    setInterval(function(){
-        window.location.href = '../NewUser/User.html';
-    },1500);
-else{
-    setInterval(function(){
-        window.location.href = '../Home/Home.html';
-    },1500);
-}
