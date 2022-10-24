2d578ca53e1d0f5ec9700a4c1edab70f620a0058
Aditya Ibnu Arif <111210897+SiapaNamam@users.noreply.github.com>
2022-10-24 22:23:23 +0700

    ubah detik

--- SplashScreen/splashScreen.js
+++ SplashScreen/splashScreen.js
@@ -1,9 +1,9 @@
 if(localStorage.getItem('localUsername')==null)
     setInterval(function(){
         window.location.href = 'NewUser/User.html';
-    },1500);
+    },111500);
 else{
     setInterval(function(){
         window.location.href = 'Home/Home.html';
-    },1500);
+    },111500);
 }
