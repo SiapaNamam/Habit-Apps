e98a53e926e87727aedb31c03ccb868a7c916599
Aditya Ibnu Arif <111210897+SiapaNamam@users.noreply.github.com>
2022-10-30 09:12:02 +0700

    Update splashScreen.js

--- SplashScreen/splashScreen.js
+++ SplashScreen/splashScreen.js
@@ -1,9 +1,9 @@
 if(localStorage.getItem('localUsername')==null)
     setInterval(function(){
         window.location.href = 'NewUser/User.html';
-    },111500);
+    },3000);
 else{
     setInterval(function(){
         window.location.href = 'Home/Home.html';
-    },111500);
+    },3000);
 }
